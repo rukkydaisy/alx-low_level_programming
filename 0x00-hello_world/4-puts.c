9d070dbf67d3f721a62ecf7f79a3e618ad8c2ce3
rukkydaisy <rukkydaisy003@gmail.com>
2023-08-10 19:54:03 +0100

    program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash 
+#Include <stdio.h>
+/**
+ * main - A program that prints a line with puts Function 
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like a multilingual puzzle");
+return (0)
+}

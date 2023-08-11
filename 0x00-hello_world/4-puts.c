bdad2b30a8fdc658522a07d31d3ea3c452038612
rukkydaisy <rukkydaisy003@gmail.com>
2023-08-10 17:05:14 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,10 @@
-<<<<<<< HEAD
 #include <stdio.h>
 /**
- * main - program that prints with puts function
- * Return:(0)
- */
-int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-=======
-#!/bin/bash 
-#Include <stdio.h>
-/**
- * main - A program that prints a line with puts Function 
+ * main - A program that prints a line with puts Function
  * Return: 0
  */
 int main(void)
 {
 puts("\"Programming is like a multilingual puzzle");
-return (0)
->>>>>>> 9d070dbf67d3f721a62ecf7f79a3e618ad8c2ce3
+return (0);
 }

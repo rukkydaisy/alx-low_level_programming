518da86bdfdd09f180ce3bd95f870df748271dc3
rukkydaisy <rukkydaisy003@gmail.com>
2023-08-12 06:57:43 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int zero = '0';
+
+	while (zero <= '9')
+	{
+		putchar(zero);
+		zero++;
+	}
+	putchar('\n');
+	return (0);
+}

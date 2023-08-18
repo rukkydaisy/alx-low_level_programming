be9d30bf91b076cede96000ff622bb7def536981
rukkydaisy <rukkydaisy003@gmail.com>
2023-08-18 12:50:20 -0700

    s

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,11 +12,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 	
-	if (a > b && b > c)
+	if (a >= b && b >= c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
+	else if (b >= a && a >= c)
 	{
 		largest = b;
 	}
@@ -25,5 +25,5 @@ int largest_number(int a, int b, int c)
 		largest = c;
 	}
 
-	return (largest);
+	return (largest'\n');
 }

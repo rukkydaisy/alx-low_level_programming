9644e27026247cd2556d3f7f6974e6a34a5b11a0
rukkydaisy <rukkydaisy003@gmail.com>
2023-08-21 16:59:36 -0700

    p

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,13 +5,20 @@
  * to by src, including the terminating
  * null byte
  *
- * *dest: a pointer
- * *src: a pointer
+ * @dest: a pointer
+ * @src: a pointer
  *
  * Return: 0
  *
  */
 char *_strcpy(char *dest, char *src)
 {
+	int i;
 
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
 }

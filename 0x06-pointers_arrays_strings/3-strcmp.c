7d8c48e5c40f1859b4de88b61b29fa162c8de721
rukkydaisy <rukkydaisy003@gmail.com>
2023-08-24 08:49:51 -0700

    h

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,15 +2,17 @@
 /**
  * _strcmp - this program compares two strings
  *
- * @si: string 1
+ * @s1: string 1
  * @s2: string 2
  *
  * Return: 0
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	int b = 0;
-
-	for (i =0; s1[i] != '\0' && b == 0; i++)
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
